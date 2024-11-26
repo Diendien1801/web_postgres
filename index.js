cd1ce9fe586bcd3aeddfde37a3147c125935f8a7
Diendien1801 <116612379+Diendien1801@users.noreply.github.com>
2024-11-26 22:30:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const { createPagination } = require("express-handlebars-paginate");
 const app = express();
-const port = process.env.NODE_ENV || 10000;
+const port =  10000;
 
 app.use(express.static(__dirname + "/public"));
 
